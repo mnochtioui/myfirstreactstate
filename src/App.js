8a8be32030b4ac1aaf646c9bee13a05cce1064bc
mnochtioui <134012404+mnochtioui@users.noreply.github.com>
2023-06-10 17:55:16 +0100

    fist cmt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import mounir from './assets/mounir.jpg'
 
-function App() {
+class App extends React.Component {
+ 
+  state={
+    "fullName": "Mounir CHTIOUI",
+    "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+    "imgSrc": {mounir}, 
+    "profession": "IT dev", 
+    "show": false ,
+    "cpt": 1
+  };
+  
+ 
+ render(){
+  var handleShow =()=>{this.setState({show:!this.state.show})};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Hello {this.state.fullName}!?</h1>
+        <button onClick={handleShow} >ClickMe</button>
+        <p>{this.state.show? 
+        <i>{this.state.bio}</i>
+        : 
+        <img src={mounir} alt="mounir" style={{width : "50px" }} />
+      }</p>
+        
     </div>
   );
+ };
+  
 }
 
 export default App;
